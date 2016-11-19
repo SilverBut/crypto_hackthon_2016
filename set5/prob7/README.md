6ae545d7a1cf17960dbc1ca335c0212524b1190c
Ricky-Hao <a471558277@gmail.com>
2016-11-19 14:11:26 +0800

    Add Challenge 39

--- set5/prob7/README.md
+++ set5/prob7/README.md
@@ -1,7 +1,21 @@
-## Problem <problem id>.<title>
+## Problem 39.Implement RSA
 
-### 0x01
+### 0x01 Question
+这题要求我们实现一个RSA加密。
 
-### 0x02
+### 0x02 Environment
+* Python 3.5 with gmpy2,pycrypto,binascii
+### 0x03 Step
+1. 使用__Crypto.Util.number.getPrime(length)__函数来产生足够长的大素数。
+2. 通过自己写的__egcd__以及__invmod__函数来找到合适的p使得`p*d=1(mod n)`。
+3. 这里采用`binascii.hexlify(message.encode())`来获取明文的hex。
+4. 对于大数的指数运算，这里使用了`gmpy2.powmod(number,exp,mod)`函数。
+5. 最终的密文经过了__base64__加密。
 
-### 0x03 Reference
+### 0x04 More
+* 实现RSA的时候出现了几个问题。首先是`m\*\*e%n`这样的大数指数运算速度太慢。最后找到了`gmpy2.powmod`函数来解决。
+* 因为一开始选择的素数长度不够，所以使用了`Crypto.Util.number.getPrime()`函数来取得需要位数的大素数。
+### 0x05 Reference
+[PyCrypto API Documentation](http://pythonhosted.org/pycrypto/)
+[Gmpy2 API Documentation](http://gmpy2.readthedocs.io/en/latest)
+
---
+++ set5/prob7/solve.py
@@ -0,0 +1,82 @@
+import random,binascii,base64
+from gmpy2 import powmod
+from Crypto.Util import number
+
+def isPrime(n):
+    if n==2 or n==3: return True
+    if n%2==0 or n<2: return False
+    for i in range(3,int(n**0.5)+1,2):   # only odd numbers
+        if n%i==0:
+            return False    
+
+    return True
+
+def egcd(a, b):
+    if(a==0):
+        return (b,0,1)
+    else:
+        g,y,x=egcd(b%a,a)
+        return (g,x-(b//a)*y,y)
+
+def invmod(e, et):
+    g,x,y=egcd(e,et)
+    if(g!=1):
+        return False
+    else:
+        return x%et
+
+def genD():
+    length=1024
+    p=number.getPrime(length)
+    q=number.getPrime(length)
+    n=q*p
+    et=(p-1)*(q-1)
+    e=3
+    d=invmod(e,et)
+
+    return (e,d,n)
+
+
+def genKey():
+    (e,d,n)=genD()
+    while(d==False):
+        (e,d,n)=genD()
+
+    return ([e,n],[d,n])
+
+def encrypt(message, public_key):
+    temp=binascii.hexlify(message.encode())
+    temp=int(temp,16)
+    temp=powmod(temp,public_key[0],public_key[1])
+    temp=int(temp.digits())
+    temp=hex(temp)[2:]
+    if(len(temp)%2!=0):
+        temp='0'+temp
+    temp=base64.b64encode(temp.encode())
+    return temp.decode()
+
+def decrypt(cipher, private_key):
+    temp=base64.b64decode(cipher.encode())
+    temp=int(temp,16)
+    temp=powmod(temp,private_key[0],private_key[1])
+    temp=int(temp.digits())
+    temp=hex(temp)[2:]
+    if(len(temp)%2!=0):
+        temp='0'+temp
+    temp=binascii.unhexlify(temp.encode())
+    return temp.decode()
+
+def run():
+    (public_key,private_key)=genKey()
+
+    print("Public Key:\ne: "+str(public_key[0])+"\nn: "+str(public_key[1]))
+    print("Private Key:\nd: "+str(private_key[0])+"\nn: "+str(private_key[1]))
+
+    message=input()
+    cipher=encrypt(message,public_key)
+    print("\nCipher: "+cipher)
+    plain=decrypt(cipher,private_key)
+    print("\nPlain: "+plain)
+
+if __name__=="__main__":
+    run()
