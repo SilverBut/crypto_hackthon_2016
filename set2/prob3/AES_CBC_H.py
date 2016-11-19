d99aef8623eae31ca71399f806fa26ca757a0fc4
Crystal <yysfs1y1@gmail.com>
2016-11-19 17:50:18 +0800

    Add files via upload

--- set2/prob3/AES_CBC_H.py
+++ set2/prob3/AES_CBC_H.py
@@ -1,5 +1,5 @@
 __author__ = 'Administrator'
-import set1.AES_ECB,set1.hex_xor,set1.HexBase64
+import AES_ECB,hex_xor,HexBase64
 
 
 def decrypt(c, key, iv):
@@ -8,8 +8,8 @@ def decrypt(c, key, iv):
         return
     else:
         for i in range(0, len(c)/16):
-            pc=set1.AES_ECB.decrypt(c[16*i:16*(i+1)], key)
-            pc = set1.hex_xor.hex_xor(iv.encode('hex'),pc.encode('hex')).decode('hex')
+            pc=AES_ECB.decrypt(c[16*i:16*(i+1)], key)
+            pc = hex_xor.hex_xor(iv.encode('hex'),pc.encode('hex')).decode('hex')
             m = m+pc
             iv= c[16*i:16*(i+1)]
         return m
@@ -20,14 +20,14 @@ def encrypt(m, key, iv):
         return
     else:
         for i in range(0, len(m)/16):
-            pc = set1.hex_xor.hex_xor(iv.encode('hex'), m[16*i:16*(i+1)].encode('hex')).decode('hex')
-            iv = set1.AES_ECB.encrypt(pc, key)
+            pc = hex_xor.hex_xor(iv.encode('hex'), m[16*i:16*(i+1)].encode('hex')).decode('hex')
+            iv = AES_ECB.encrypt(pc, key)
             c = c+iv
         return c
 
 if __name__ == '__main__':
     f = open('10.txt', 'r')
-    c = set1.HexBase64.base642hex(f.read()).decode('hex')
+    c = HexBase64.base642hex(f.read()).decode('hex')
     key = 'YELLOW SUBMARINE'
     iv = chr(0)*16
     print decrypt(c, key, iv)
