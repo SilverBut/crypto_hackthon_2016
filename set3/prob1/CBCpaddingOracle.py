4c9f9b6479e44629c39db589e04a102678fad36c
Crystal <yysfs1y1@gmail.com>
2016-11-19 22:00:30 +0800

    Add files via upload

--- set3/prob1/CBCpaddingOracle.py
+++ set3/prob1/CBCpaddingOracle.py
@@ -1,19 +1,19 @@
 __author__ = 'Administrator'
 import random
-import set2.AES_CBC_H,set2.R_ECB_CBC,set2.pkcs7
+import AES_CBC_H,R_ECB_CBC,pkcs7
 
-key=set2.R_ECB_CBC.random_key(16)
-iv=set2.R_ECB_CBC.random_key(16)
+key=R_ECB_CBC.random_key(16)
+iv=R_ECB_CBC.random_key(16)
 
 
 def encrypto(m):
-    return set2.AES_CBC_H.encrypt(set2.pkcs7.pkcs7(m,16),key,iv)
+    return AES_CBC_H.encrypt(pkcs7.pkcs7(m,16),key,iv)
 
 
 def checkCipher(c):
     try:
-        c = set2.pkcs7.d_pkcs7(set2.AES_CBC_H.decrypt(c[16:],key,c[0:16]),16)
-    except set2.pkcs7.PadError as e:
+        c = pkcs7.d_pkcs7(AES_CBC_H.decrypt(c[16:],key,c[0:16]),16)
+    except pkcs7.PadError as e:
         return False
     return True
 
